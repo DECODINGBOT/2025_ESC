509bdc1cf3d6c079f7c5b0660d0f8baa73954383
coders1019 <daniel3898@naver.com>
2025-08-15 16:13:22 +0900

    comission

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // Flutter 플러그인은 Android/Kotlin 이후에 와야 합니다.
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
 }
@@ -9,7 +8,6 @@ plugins {
 android {
     namespace = "com.example.sharing_items"
 
-    // Flutter 변수 대신 명시값으로 고정 (권장)
     compileSdk = 35
 
     defaultConfig {
@@ -17,12 +15,10 @@ android {
         minSdk = 21
         targetSdk = 34
 
-        // Flutter가 생성해둔 값 유지하려면 그대로 사용
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
-    // (선택) NDK 버전이 필요할 때만 사용
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -36,7 +32,6 @@ android {
 
     buildTypes {
         release {
-            // 데모용으로 debug 키로 서명 (실서비스 시 release 서명키 설정)
             signingConfig = signingConfigs.getByName("debug")
         }
     }
